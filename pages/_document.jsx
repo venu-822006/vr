de07c51ed6b141eb66f1ef49f8f9a40cbe8fc5c9
venu <venugopalreddyk392@gmail.com>
2026-08-02 14:21:56 +0530

    Add favicon to _document.jsx for Google Search results

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -6,6 +6,8 @@ export default function Document() {
     <Html lang="en" translate="no">
       <Head>
         <meta name="google" content="notranslate" />
+        <link rel="icon" href="/icons/icon-192.png" type="image/png" />
+        <link rel="apple-touch-icon" href="/icons/icon-192.png" />
       </Head>
       <body style={{ margin: 0, padding: 0 }}>
         <Main />
